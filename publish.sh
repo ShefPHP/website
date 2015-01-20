16e20b749a3bb11ed62dad1c0770148ee2fe6faf
Stuart Grimshaw <stuart.grimshaw@wandisco.com>
2015-01-20 10:36:24 +0000

    Added a publish script for the site.

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+cd output_prod/ && git add . && git commit -m "$1" && git push
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
